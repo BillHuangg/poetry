c342e612cdf2f4248e0af35ede7e4ecfc419d388
billhuang <billhuangg@gmail.com>
2017-02-08 01:09:32 +0800

    [Update]: init project

---
+++ poetry/poetry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  poetry
+//
+//  Created by bill on 17/2/7.
+//  Copyright © 2017年 bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
