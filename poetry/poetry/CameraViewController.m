75f0d6ace22df6b3c0605577229fb099846a3555
billhuang <billhuangg@gmail.com>
2017-02-12 22:43:23 +0800

    [Update]: add photo capture

--- poetry/poetry/CameraViewController.m
+++ poetry/poetry/CameraViewController.m
@@ -8,6 +8,8 @@
 
 #import "CameraViewController.h"
 #import "NSObject+FormatLog.h"
+#import "Constant.h"
+#import "MBProgressHUD.h"
 #import "GPUImage.h"
 
 #define CameraViewMarginTop 44
@@ -24,7 +26,12 @@ typedef enum {
     
     UIButton *_toggleCameraRectButton;
     UIButton *_toggleCameraRotationButton;
+    UIButton *_toggleCameraFlashButton;
     
+    UIButton *_photoCaptureButton;
+    UIButton *_albumButton;
+    UIButton *_filterButton;
+
     CameraViewRectType _currentCameraViewRectType;
 }
 @end
@@ -43,6 +50,7 @@ typedef enum {
     [super didReceiveMemoryWarning];
 }
 
+# pragma mark - Camera Setup
 - (void)setupCameraView {
     
     // init as CAMERA_VIEW_RECT_VIEW_11
@@ -56,15 +64,6 @@ typedef enum {
     [self.view addSubview:_cameraView];
 }
 
-- (void)setupButtons {
-    
-    _toggleCameraRectButton = [self generateButton:@"rect" rect:CGRectMake(20, 20, 60, 40) action:@selector(toggleCameraRectEvent)];
-    [self.view addSubview:_toggleCameraRectButton];
-    
-    _toggleCameraRotationButton = [self generateButton:@"rotation" rect:CGRectMake(100, 20, 60, 40) action:@selector(toggleCameraRotationEvent)];
-    [self.view addSubview:_toggleCameraRotationButton];
-}
-
 - (void)setupCamera {
     
     _videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPresetPhoto cameraPosition:AVCaptureDevicePositionBack];
@@ -75,6 +74,50 @@ typedef enum {
     
     [_videoCamera addTarget:_cameraView];
     [_videoCamera startCameraCapture];
+}
+
+# pragma mark - View And Animation Setup
+- (void)setupButtons {
+    
+    [self setupTopButtons];
+    [self setupBottomButtons];
+}
+
+
+- (void) setupTopButtons {
+    
+    // toggle camera rect
+    _toggleCameraRectButton = [self generateButton:@"rect" rect:CGRectMake(20, 20, 60, 40) action:@selector(toggleCameraRectEvent)];
+    [self.view addSubview:_toggleCameraRectButton];
+    
+    // toggle camera rotation
+    _toggleCameraRotationButton = [self generateButton:@"rotation" rect:CGRectMake(100, 20, 60, 40) action:@selector(toggleCameraRotationEvent)];
+    [self.view addSubview:_toggleCameraRotationButton];
+
+    // toggle flash
+    _toggleCameraFlashButton = [self generateButton:@"flash" rect:CGRectMake(180, 20, 60, 40) action:@selector(toggleCameraFlashEvent)];
+    [self.view addSubview:_toggleCameraFlashButton];
+    
+}
+
+- (void) setupBottomButtons {
+    
+    float x = ScreenWidth / 2 - 40;
+    float y = (ScreenHeight + _cameraView.frame.size.height + _cameraView.frame.origin.y) / 2 - 40;
+    
+    // open album
+    _albumButton = [self generateButton:@"album" rect:CGRectMake(x - 100, y, 80, 80) action:@selector(albumOpenEvent)];
+    [self.view addSubview:_albumButton];
+    
+    // take picture
+    _photoCaptureButton = [self generateButton:@"" rect:CGRectMake(x, y, 80, 80) action:@selector(photoCaptureEvent)];
+    _photoCaptureButton.backgroundColor = [UIColor blackColor];
+    _photoCaptureButton.layer.cornerRadius = 40;
+    [self.view addSubview:_photoCaptureButton];
+    
+    // filter view
+    _filterButton = [self generateButton:@"filter" rect:CGRectMake(x + 100, y, 80, 80) action:@selector(filterOpenEvent)];
+    [self.view addSubview:_filterButton];
 }
 
 - (UIButton*)generateButton:(NSString*) name rect:(CGRect) rect action:(SEL)action {
@@ -92,6 +135,7 @@ typedef enum {
     return button;
 }
 
+# pragma mark - Button Event
 - (void)toggleCameraRectEvent {
     
     CGRect rectStatus = [[UIApplication sharedApplication] statusBarFrame];
@@ -115,9 +159,79 @@ typedef enum {
 
 - (void)toggleCameraRotationEvent {
     
+    // TODO: front, mirrored
     [_videoCamera rotateCamera];
     
     [self FormatLogWithClassNameAndMessage:@"toggleCameraRotationEvent"];
 }
+
+- (void)toggleCameraFlashEvent {
+    
+    [self FormatLogWithClassNameAndMessage:@"toggleCameraFlashEvent"];
+}
+
+- (void)photoCaptureEvent {
+    
+    [_videoCamera useNextFrameForImageCapture];
+    [self saveCameraPhotoToAlbum: _videoCamera.imageFromCurrentFramebuffer];
+    [self FormatLogWithClassNameAndMessage:@"photoCaptureEvent"];
+}
+
+- (void)albumOpenEvent {
+    
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.delegate = self;
+    imagePickerController.allowsEditing = NO;
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    
+    [self presentViewController:imagePickerController animated:YES completion:nil];
+    
+    [self FormatLogWithClassNameAndMessage:@"albumOpenEvent"];
+}
+
+- (void)filterOpenEvent {
+    
+    [self FormatLogWithClassNameAndMessage:@"filterOpenEvent"];
+}
+
+# pragma mark - Logic
+
+# pragma mark - Functions
+
+# pragma mark - UIImagePickerController Delegate
+- (void)saveCameraPhotoToAlbum:(UIImage *)image {
+    
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
+}
+
+- (void)image: (UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
+    
+    NSString *message = IsNilOrNull(error) ? @"保存成功" : @"保存失败";
+    
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.mode = MBProgressHUDModeText;
+    hud.label.text = message;
+    hud.removeFromSuperViewOnHide = YES;
+    [hud hideAnimated:YES afterDelay:1.5f];
+}
+
+# pragma mark - UIImagePickerController Delegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    [self FormatLogWithClassNameAndMessage:__String(@"Get Image, name: %@", image.description)];
+    
+    // TODO: go to photo edit view
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+
+    [self FormatLogWithClassNameAndMessage:@"imagePickerControllerDidCancel"];
+}
+
 
 @end
---
+++ poetry/poetry/Constant.h
@@ -0,0 +1,34 @@
+//
+//  Constant.h
+//  poetry
+//
+//  Created by bill on 17/2/12.
+//  Copyright © 2017年 bill. All rights reserved.
+//
+
+#define ScreenHeight [[UIScreen mainScreen] bounds].size.height
+#define ScreenWidth  [[UIScreen mainScreen] bounds].size.width
+
+#define NotNilAndNull(_ref)  (((_ref) != nil) && (![(_ref) isEqual:[NSNull null]]))
+#define IsNilOrNull(_ref)   (((_ref) == nil) || ([(_ref) isEqual:[NSNull null]]))
+
+#define IsStringEmpty(_ref)    (((_ref) == nil) || ([(_ref) isEqual:[NSNull null]]) ||([(_ref)isEqualToString:@""]))
+#define IsArrayEmpty(_ref)    (((_ref) == nil) || ([(_ref) isEqual:[NSNull null]]) ||([(_ref) count] == 0))
+
+
+//便捷方式创建NSNumber类型
+#undef	__INT
+#define __INT( __x )			[NSNumber numberWithInt:(NSInteger)__x]
+
+#undef	__UINT
+#define __UINT( __x )			[NSNumber numberWithUnsignedInt:(NSUInteger)__x]
+
+#undef	__FLOAT
+#define	__FLOAT( __x )			[NSNumber numberWithFloat:(float)__x]
+
+#undef	__DOUBLE
+#define	__DOUBLE( __x )			[NSNumber numberWithDouble:(double)__x]
+
+//便捷创建NSString
+#undef  __String
+#define __String( __x, __y )    [NSString stringWithFormat:__x, __y]
